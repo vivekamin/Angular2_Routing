820f069c0f48267f017b0b8f601ccf9d46feae7b
vivekamin <vivek@yodaplus.com>
2017-03-29 19:04:26 +0530

    Simple Routing Implemented.milestone1 achieved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule , Routes } from '@angular/router';
+
+
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list.component';
+import { HeroListComponent } from './hero-list.component';
+import { PageNotFoundComponent } from './not-found.component';
+
+const appRoutes : Routes = [
+  { path: 'crisis-center' , component : CrisisListComponent },
+  { path: 'heroes' , component: HeroListComponent },
+
+  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroListComponent,
+    CrisisListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hero-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template : ` <h2>HEROES CENTER</h2>
+    <p>Get your heroes here</p>`
+})
+
+export class HeroListComponent {
+  
+}
